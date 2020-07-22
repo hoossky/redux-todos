a8e588b7e156cecf38be0da045e699d3f99579a5
이호섭 <62980969+hoossky@users.noreply.github.com>
2020-07-22 19:01:03 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import TodoList from "./schedule/TodoList";
+import TodoInput from "./schedule/TodoInput";
+import {Provider} from 'react-redux'
+import store from "./schedule/store/index"
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+
+const App = () => {
+  return <>
+      <Provider store = {store}>
+        <TodoInput/><br/><TodoList/>
+      </Provider>
+      </>
+
+}
+export default App;
